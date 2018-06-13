efe936e5659cde0de0058fab214a81c580284cd1
Hannah Werman <hanna.w28@gmail.com>
2018-06-13 09:50:35 -0500

    fixed student view

--- src/components/StudentNav.js
+++ src/components/StudentNav.js
@@ -4,9 +4,12 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 const StudentNav = ({toggleStudentView}) => {
   return (
-    <div className="button-group">
-      <RaisedButton className="wide-button" primary={true} onClick={toggleStudentView}>Course Materials</RaisedButton>
-      <RaisedButton className="wide-button" primary={true} onClick={toggleStudentView}>Settings</RaisedButton>
+    <div>
+      <div className="buffer" />
+      <div className="button-group">
+        <RaisedButton className="wide-button" primary={true} onClick={toggleStudentView}>Course Materials</RaisedButton>
+        <RaisedButton className="wide-button" primary={true} onClick={toggleStudentView}>Settings</RaisedButton>
+      </div>
     </div>
   )
 }
