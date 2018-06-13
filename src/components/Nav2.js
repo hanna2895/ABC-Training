5fcad3028c53289be0157729a2bf5813b870aded
Hannah Werman <hanna.w28@gmail.com>
2018-06-12 20:37:27 -0500

    fixed navbar for walled-off site bits

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -3,11 +3,14 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 const Nav2 = ({clickStudents, clickGroups, clickSettings, clickCourseMaterials}) => {
   return (
-    <div className="button-group">
-      <RaisedButton className="wide-button" primary={true} onClick={clickGroups}>Clients and Groups</RaisedButton>
-      <RaisedButton className="wide-button" primary={true} onClick={clickStudents}>Students</RaisedButton>
-      <RaisedButton className="wide-button" primary={true} onClick={clickCourseMaterials}>Course Materials</RaisedButton>
-      <RaisedButton className="wide-button" primary={true} onClick={clickSettings}>Settings</RaisedButton>
+    <div>
+      <div className="buffer"/>
+      <div className="button-group">
+        <RaisedButton className="wide-button" primary={true} onClick={clickGroups}>Clients and Groups</RaisedButton>
+        <RaisedButton className="wide-button" primary={true} onClick={clickStudents}>Students</RaisedButton>
+        <RaisedButton className="wide-button" primary={true} onClick={clickCourseMaterials}>Course Materials</RaisedButton>
+        <RaisedButton className="wide-button" primary={true} onClick={clickSettings}>Settings</RaisedButton>
+      </div>
     </div>
   )
 }
